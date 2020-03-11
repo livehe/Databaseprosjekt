04ca71ee0dfd75a4a1ea016ccbf5fabb38ba7384
mldnorheim <mldnorheim@gmail.com>
2020-03-11 16:03:41 +0100

    minitillegg for å teste push

---
+++ src/GetInfo.java
@@ -0,0 +1,17 @@
+package databaseprosjekt;
+
+import java.sql.*; // Connection, ResultSet, SQLException, Statement
+import java.util.*; // List, ArrayList, Scanner
+
+public class GetInfo {
+    
+    private Connection conn; // vår kopling til databasen
+
+    public GetInfo() {
+
+    }
+
+
+
+}
+
