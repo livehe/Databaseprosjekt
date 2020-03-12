a89a55e4315614199ba60eb4c3c4e53d62cd4fe6
mldnorheim <35527310+mldnorheim@users.noreply.github.com>
2020-03-12 20:33:53 +0100

    Delete ActiveDomainObjekt.java

--- src/ActiveDomainObjekt.java
+++
@@ -1,8 +0,0 @@
-
-import java.sql.*;
-
-public abstract class ActiveDomainObjekt {
-    public abstract void initialize (Connection conn);
-    public abstract void refresh (Connection conn);
-    public abstract void save (Connection conn);
-}
